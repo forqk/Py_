43b67e5c4c7dfb84f8bb77aa9d9aeef2094776de
forqk <educationfork@gmail.com>
2022-12-29 15:57:55 +0300

    fix

---
+++ 1.Basics/FindEmptyList/solution.py
@@ -0,0 +1,27 @@
+
+list_a = [1,2,3,4]
+
+list_b = [1,]
+
+list_c = []
+
+list_d = [[]]
+
+list_e = list()
+
+print(len(list_c))
+print(len(list_d))
+print(len(list_e))
+print(len(list_a * 0))
+#print(len(list_b.pop()))
+#print(len([_ for i in list_b]))
+print(len(list_a[-1:0]))
+list_a.clear(); print(len(list_a)) 
+
+l = ['a', 'b', 'c', 'd'] # список
+k = ('a', 'b', 'c', 'd', 'e') # кортеж
+s = set("abcdefg") #множество
+d = dict(); #словарь
+
+print((s))
+
