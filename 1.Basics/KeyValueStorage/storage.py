220e6fc2ac0fd2de58b35c115eb019f1212fde3f
forqk <educationfork@gmail.com>
2022-12-29 12:38:06 +0300

    func

--- 1.Basics/KeyValueStorage/storage.py
+++ 1.Basics/KeyValueStorage/storage.py
@@ -4,30 +4,41 @@ import tempfile
 import os
 from pathlib import Path
 
-parser = argparse.ArgumentParser()
+def Parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--key", type = str)
+    parser.add_argument("--value", type = str)
+    return parser.parse_args()
 
-parser.add_argument("--key", type = str)
-parser.add_argument("--value", type = str)
-
-args = parser.parse_args()
-
-my_dict = dict()
-
-storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-
-if os.path.isfile(Path(storage_path)):
-    with open(storage_path, 'r') as f: 
-        my_dict = json.loads(f.read())
-            
-
-with open(storage_path, 'w') as f:    
-    if args.key and args.value:
-        my_dict.setdefault(args.key, [])
-        my_dict[args.key].append(args.value)
+def ReadFromFile(storage_path) -> dict:
+    if not os.path.isfile(Path(storage_path)):
+        return {}
+    with open(storage_path, 'r') as f:
+        line = f.read()
+        if(len(line) > 0):
+            return json.loads(line)
+        
+def WriteToFile(my_dict, storage_path, key, value):
+    with open(storage_path, 'w+') as f:            
+        my_dict.setdefault(key, [])
+        my_dict[key].append(value)
         f.write(json.dumps(my_dict))  
-             
-    if args.key and not args.value: # считал и ничего не записал обратно, после этой строчки очищается файл.
-        print("--value empty")
-        if args.key not in my_dict:
-            print(None)
+         
+def PutToFile(storagepath, key, value):
+    my_dict = ReadFromFile(storagepath)
+    WriteToFile(my_dict, storagepath, key, value)
             
+def GetData(storagepath, key):
+    my_dict = ReadFromFile(storagepath)
+    return my_dict.get(key, [])
+    
+def main():
+    args = Parse()
+    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+    
+    if (args.key and args.value):
+        PutToFile(storage_path, args.key, args.value)
+    elif args.key and not args.value :
+        print(*GetData(storage_path, args.key), sep = ', ')
+        
+main()
