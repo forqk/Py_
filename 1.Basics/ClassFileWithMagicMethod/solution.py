352f4159cb6317ed5462cc2782a438b4e4ed584c
forqk <educationfork@gmail.com>
2023-02-03 17:00:19 +0300

    fix

--- 1.Basics/ClassFileWithMagicMethod/solution.py
+++ 1.Basics/ClassFileWithMagicMethod/solution.py
@@ -6,6 +6,9 @@ base_path = tempfile.gettempdir()
 
 
 class File:
+    line_count = 0
+    iter_count = 0
+
     def __init__(self, pathname):
         self.pathname = pathname
 
@@ -20,12 +23,26 @@ class File:
         new_file.write(self.read() + obj.read())
         return new_file
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.iter_count > self.line_count:
+            raise StopIteration 
+
+        with open(os.path.join(base_path, self.pathname), 'r') as file:
+            for num_line, line in enumerate(file):
+                if num_line == self.iter_count:
+                    self.iter_count += 1
+                    return line
+
     def read(self):
         with open(os.path.join(base_path, self.pathname), 'r') as file:
             return file.read()
 
     def write(self, line):
         with open(os.path.join(base_path, self.pathname), 'w+') as file:
+            self.line_count += 1
             return file.write(line)
 
 
@@ -49,5 +66,6 @@ def main():
     print(new_file_obj)
     for line in new_file_obj:
         print(ascii(line))
+
 
 main()
