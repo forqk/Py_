542fe361e4608ac2ff3de736e13951ec4b9561f1
forqk <educationfork@gmail.com>
2023-02-06 22:05:51 +0300

    Iterators

--- 1.Basics/ClassFileWithMagicMethod/solution.py
+++ 1.Basics/ClassFileWithMagicMethod/solution.py
@@ -1,59 +1,55 @@
 import os.path
 import tempfile
-import sys
 
 base_path = tempfile.gettempdir()
 
-
 class File:
-    line_count = 0
-    iter_count = 0
-
     def __init__(self, pathname):
         self.pathname = pathname
-
-        with open(os.path.join(base_path, pathname), 'a+'):
-            pass
-
-    def __str__(self):
+        self.current_position = 0;
+        
+        if not os.path.exists(pathname):
+            with open(os.path.join(pathname), 'a+'):
+                pass
+            
+    def __str__(self): 
         return self.pathname
 
     def __add__(self, obj):
-        new_file = File(os.path.join(base_path, "new_file"))
+        new_file = File(os.path.join(base_path, tempfile.NamedTemporaryFile().name))
         new_file.write(self.read() + obj.read())
         return new_file
 
     def __iter__(self):
         return self
-
+            
     def __next__(self):
-        if self.iter_count > self.line_count:
-            raise StopIteration 
-
-        with open(os.path.join(base_path, self.pathname), 'r') as file:
-            for num_line, line in enumerate(file):
-                if num_line == self.iter_count:
-                    self.iter_count += 1
-                    return line
+        with open(self.pathname, 'r') as file:
+            file.seek(self.current_position)
+            line = file.readline()
+            if not line:
+                self.current_position = 0;
+                raise StopIteration
+            self.current_position = file.tell()
+            return line
                 
     def __str__(self):
-        return os.path.join(base_path, self.pathname)
+        return self.pathname
 
     def read(self):
-        with open(os.path.join(base_path, self.pathname), 'r') as file:
+        with open(self.pathname, 'r') as file:
             return file.read()
 
     def write(self, line):
-        with open(os.path.join(base_path, self.pathname), 'w+') as file:
-            self.line_count += 1
+        with open(self.pathname, 'w') as file:
             return file.write(line)
 
 
 def main():
     path_to_file = 'some_filename'
-    print(os.path.exists(os.path.join(base_path, path_to_file)))
+    print(os.path.exists(path_to_file))
     file_obj = File(path_to_file)
-    print(os.path.exists(os.path.join(base_path, path_to_file)))
+    print(os.path.exists(path_to_file))
     print(file_obj)
     print(file_obj.read())
     print(file_obj.write('some text'))
@@ -74,5 +70,12 @@ def main():
     print(os.path.exists(new_path_to_file))
     file_obj_3 = File(new_path_to_file)
     print(file_obj_3)
-
+    with open('some_file.txt', 'w') as file:
+        file.write('1\n2\n3\n')
+    print('файл существует? -', os.path.exists('some_file.txt'))
+    file = File('some_file.txt')
+    print('содержание файла -', ascii(file.read()))
+    print('содержание файла -')
+    for row in file:
+        print('строка -', ascii(row))
 main()
