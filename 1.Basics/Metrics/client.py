f256cada60d52345b929532f221be7a9e7fa94d9
forqk <educationfork@gmail.com>
2023-03-02 17:22:52 +0300

    client server

--- 1.Basics/Metrics/client.py
+++ 1.Basics/Metrics/client.py
@@ -1,5 +1,9 @@
 import socket
 import time
+import json
+
+class ClientError(Exception):
+    pass
 
 class Client:
     socket_ = None
@@ -8,15 +12,33 @@ class Client:
         self.host = host;
         self.port = port
         self.timeout = timeout
-        self.CreateConnection()
         
-    def CreateConnection(self):
         self.socket_ = socket.create_connection((self.host, self.port))
+        self.socket_.settimeout(self.timeout)
+        
         
     #<команда> <данные запроса><\n>
-    def put(self, command, date, timestamp = int(time.time())):
-        line_ = command + " " + str(date) + " " +  str(timestamp) + '\n'
-        self.socket_.sendall(line_.encode("utf8"))
+    def put(self, metric, value, timestamp = int(time.time())):
+        line_ = metric + " " + str(value) + " " +  str(timestamp) + '\n'
+        try:
+            self.socket_.sendall(line_.encode("utf8"))
+        except:
+            raise ClientError
+        
+    def get(self, metric):
+        line_ = str(metric) + '\n'
+        try:
+            self.socket_.sendall(line_.encode("utf8"))
+            data = self.socket_.recv(1024);
+        except:
+            raise ClientError
+        print(data)
+        return data.decode()
+    
+    def __del__(self):
+        if not self.socket_ is None:
+            self.socket_.close()
+        
         
 
 def main():
@@ -28,4 +50,5 @@ def main():
     client.put("eardrum.cpu", 3, timestamp=1150864250)
     client.put("eardrum.cpu", 4, timestamp=1150864251)
     client.put("eardrum.memory", 4200000)
+    print(client.get("*"))
 main()
