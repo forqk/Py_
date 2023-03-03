cf2968554047fb5d6ac1e3c64e8fdc27231146c8
forqk <educationfork@gmail.com>
2023-03-03 14:24:32 +0300

    sockets

--- 1.Basics/Metrics/client.py
+++ 1.Basics/Metrics/client.py
@@ -1,6 +1,7 @@
 import socket
 import time
 import json
+from collections import defaultdict
 
 class ClientError(Exception):
     pass
@@ -12,7 +13,6 @@ class Client:
         self.host = host;
         self.port = port
         self.timeout = timeout
-        
         self.socket_ = socket.create_connection((self.host, self.port))
         self.socket_.settimeout(self.timeout)
         
@@ -26,14 +26,35 @@ class Client:
             raise ClientError
         
     def get(self, metric):
-        line_ = str(metric) + '\n'
+        line_ = 'get ' + str(metric) + '\n'
         try:
             self.socket_.sendall(line_.encode("utf8"))
             data = self.socket_.recv(1024);
         except:
             raise ClientError
-        print(data)
-        return data.decode()
+              
+        if data == 'ok\n\n':
+            return {}
+        
+        data = data.decode()
+        
+        if data.startswith('error'):
+            return ClientError
+        
+        data = data[3:len(data)-2:] #remove ok\n and \n\n at the end 
+        
+        result_dict = {}
+        
+        for line in data.split('\n'):
+            list_ = line.split(' ')
+            result_dict.setdefault(list_[0],[]).\
+                append((int(list_[2]), float(list_[1])))
+            
+            
+        for key, value in result_dict.items():
+            value.sort(key=lambda x: x[1])
+            
+        return result_dict
     
     def __del__(self):
         if not self.socket_ is None:
@@ -41,14 +62,14 @@ class Client:
         
         
 
-def main():
-    client = Client("127.0.0.1", 8888, timeout=15)
-    client.put("palm.cpu", 0.5, timestamp=1150864247)
-    client.put("palm.cpu", 0.5, timestamp=1150864247)
-    client.put("palm.cpu", 2.0, timestamp=1150864248)
-    client.put("palm.cpu", 0.5, timestamp=1150864248)
-    client.put("eardrum.cpu", 3, timestamp=1150864250)
-    client.put("eardrum.cpu", 4, timestamp=1150864251)
-    client.put("eardrum.memory", 4200000)
-    print(client.get("*"))
-main()
+# def main():
+#     client = Client("127.0.0.1", 8888, timeout=15)
+#     client.put("palm.cpu", 0.5, timestamp=1150864247)
+#     client.put("palm.cpu", 0.5, timestamp=1150864247)
+#     client.put("palm.cpu", 2.0, timestamp=1150864248)
+#     client.put("palm.cpu", 0.5, timestamp=1150864248)
+#     client.put("eardrum.cpu", 3, timestamp=1150864250)
+#     client.put("eardrum.cpu", 4, timestamp=1150864251)
+#     client.put("eardrum.memory", 4200000)
+#     print(client.get("*"))
+# main()
