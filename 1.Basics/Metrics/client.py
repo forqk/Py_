6b3f46b93d942ad07df8b5d673f142527a61c65c
forqk <educationfork@gmail.com>
2023-03-04 18:09:05 +0300

    Ready Clients

--- 1.Basics/Metrics/client.py
+++ 1.Basics/Metrics/client.py
@@ -1,75 +1,83 @@
+import bisect
 import socket
 import time
-import json
-from collections import defaultdict
+
 
 class ClientError(Exception):
+    """класс исключений клиента"""
     pass
 
+
 class Client:
-    socket_ = None
-    
-    def __init__(self, host, port, timeout = None):
-        self.host = host;
+    def __init__(self, host, port, timeout=None):
+        self.host = host
         self.port = port
         self.timeout = timeout
-        self.socket_ = socket.create_connection((self.host, self.port))
-        self.socket_.settimeout(self.timeout)
-        
-        
-    #<команда> <данные запроса><\n>
-    def put(self, metric, value, timestamp = int(time.time())):
-        line_ = metric + " " + str(value) + " " +  str(timestamp) + '\n'
-        try:
-            self.socket_.sendall(line_.encode("utf8"))
-        except:
-            raise ClientError
-        
-    def get(self, metric):
-        line_ = 'get ' + str(metric) + '\n'
-        try:
-            self.socket_.sendall(line_.encode("utf8"))
-            data = self.socket_.recv(1024);
-        except:
-            raise ClientError
-              
-        if data == 'ok\n\n':
-            return {}
-        
-        data = data.decode()
-        
-        if data.startswith('error'):
-            return ClientError
-        
-        data = data[3:len(data)-2:] #remove ok\n and \n\n at the end 
-        
-        result_dict = {}
-        
-        for line in data.split('\n'):
-            list_ = line.split(' ')
-            result_dict.setdefault(list_[0],[]).\
-                append((int(list_[2]), float(list_[1])))
-            
-            
-        for key, value in result_dict.items():
-            value.sort(key=lambda x: x[1])
-            
-        return result_dict
-    
-    def __del__(self):
-        if not self.socket_ is None:
-            self.socket_.close()
-        
-        
 
-# def main():
-#     client = Client("127.0.0.1", 8888, timeout=15)
-#     client.put("palm.cpu", 0.5, timestamp=1150864247)
-#     client.put("palm.cpu", 0.5, timestamp=1150864247)
-#     client.put("palm.cpu", 2.0, timestamp=1150864248)
-#     client.put("palm.cpu", 0.5, timestamp=1150864248)
-#     client.put("eardrum.cpu", 3, timestamp=1150864250)
-#     client.put("eardrum.cpu", 4, timestamp=1150864251)
-#     client.put("eardrum.memory", 4200000)
-#     print(client.get("*"))
-# main()
+        try:
+            self.connection = socket.create_connection((host, port), timeout)
+        except socket.error as err:
+            raise ClientError("Cannot create connection", err)
+
+    def _read(self):
+
+        data = b""
+
+        while not data.endswith(b"\n\n"):
+            try:
+                data += self.connection.recv(1024)
+            except socket.error as err:
+                raise ClientError("Error reading data from socket", err)
+
+        return data.decode('utf-8')
+
+    def _send(self, data):
+
+        try:
+            self.connection.sendall(data)
+        except socket.error as err:
+            raise ClientError("Error sending data to server", err)
+
+    def put(self, key, value, timestamp=None):
+
+        timestamp = timestamp or int(time.time())
+        self._send(f"put {key} {value} {timestamp}\n".encode())
+        raw_data = self._read()
+
+        if raw_data == 'ok\n\n':
+            return
+        raise ClientError('Server returns an error')
+
+    def get(self, key):
+
+        self._send(f"get {key}\n".encode())
+        raw_data = self._read()
+        data = {}
+        status, payload = raw_data.split("\n", 1)
+        payload = payload.strip()
+
+        if status != 'ok':
+            raise ClientError('Server returns an error')
+
+        if payload == '':
+            return data
+
+        try:
+
+            for row in payload.splitlines():
+                key, value, timestamp = row.split()
+                if key not in data:
+                    data[key] = []
+                bisect.insort(data[key], ((int(timestamp), float(value))))
+
+        except Exception as err:
+            raise ClientError('Server returns invalid data', err)
+
+        return data
+
+    def close(self):
+
+        try:
+            self.connection.close()
+        except socket.error as err:
+            raise ClientError("Error. Do not close the connection", err)
